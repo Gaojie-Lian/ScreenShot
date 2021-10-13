4e40462baa497c8741d1e5f08aca6592f2720981
Gaojie Lian <61766684+Raphael0401@users.noreply.github.com>
2021-10-13 12:29:43 -0400

    ScreenShot function.

---
+++ ScreenShotFunction.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ScreenShotFunction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScreenShot()
+    {
+        Texture2D CaptureScreenShot()
+        {
+            //Create a new rectangle with the screen as a reference.
+            Rect rect = new Rect(Screen.width * 0f, Screen.height * 0f, Screen.width * 1f, Screen.height * 1f);
+
+            //Convert the rectangle to Texture 2D.
+            Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGB24, false);
+
+            //Generate PNG image
+            screenShot.ReadPixels(rect, 0, 0);
+            screenShot.Apply();
+
+            byte[] bytes = screenShot.EncodeToPNG();
+
+            //Select the save path
+            var path = EditorUtility.SaveFilePanel(
+                "Save ScreenShot as PNG",
+                "",
+                "Screenshot",
+                "png");
+
+            //Makefile.
+            if (path.Length != 0)
+            {
+                if (bytes != null)
+                {
+                    File.WriteAllBytes(path, bytes);
+                }
+                Debug.Log(string.Format("ScreenShot: {0}", path));
+            }
+
+            return screenShot;
+        }
+    }
+
+}
